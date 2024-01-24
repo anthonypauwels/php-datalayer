71a296ae02a8873b76cad7056003e1e68464eedd
Anthony Pauwels <anthony.pauwels.be@gmail.com>
2024-01-24 23:01:21 +0100

    fix: wrong condition

--- src/DataLayerHandler.php
+++ src/DataLayerHandler.php
@@ -167,7 +167,7 @@ class DataLayerHandler
      */
     public function script(?string $gtm_id = null):void
     {
-        $gtm_id = !$gtm_id ?: $this->google_id;
+        $gtm_id = $gtm_id ?: $this->google_id;
 
         ?><!-- Google Tag Manager -->
         <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
@@ -203,7 +203,7 @@ class DataLayerHandler
      */
     public function noScript(?string $gtm_id = null):void
     {
-        $gtm_id = !$gtm_id ?: $this->google_id;
+        $gtm_id = $gtm_id ?: $this->google_id;
 
         ?><!-- Google Tag Manager (noscript) -->
         <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=<?= $gtm_id; ?>" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
